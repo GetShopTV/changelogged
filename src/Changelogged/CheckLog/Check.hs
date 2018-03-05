742c9af4640c37b7602333554ecc595ac727a2ee
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-03-05 14:55:38 +0300

    Ignore ignored files (when watch_files is empty)

--- src/Changelogged/CheckLog/Check.hs
+++ src/Changelogged/CheckLog/Check.hs
@@ -21,9 +21,9 @@ import Changelogged.Git
 
 -- |This is actually part if '@Main@'
 -- Check local changelog - local means what changelog is specific and has some indicator file. If file is changed changelog must change.
-checkLocalChangelogF :: WarningFormat -> Bool -> GitInfo -> FilePath -> Maybe [FilePath] -> IO Bool
-checkLocalChangelogF fmt writeLog GitInfo{..} path watchFilePaths = do
-  info $ "looking for missing entries in " <> format fp path
+checkLocalChangelogF :: WarningFormat -> Bool -> GitInfo -> ChangelogConfig -> IO Bool
+checkLocalChangelogF fmt writeLog GitInfo{..} ChangelogConfig{..} = do
+  info $ "looking for missing entries in " <> format fp changelogChangelog
   
   commits <- map (fromJustCustom "Cannot find commit hash in git log entry" . hashMatch . lineToText)
     <$> fold (select gitHistory) Fold.list
@@ -32,8 +32,15 @@ checkLocalChangelogF fmt writeLog GitInfo{..} path watchFilePaths = do
   return $ and flags
   where
     eval commit = do
-      ignoreChange <- case watchFilePaths of
-        Nothing -> return False
+      ignoreChange <- case changelogWatchFiles of
+        Nothing -> case changelogIgnoreFiles of
+          Nothing -> return False
+          Just files -> do
+            linePresent <- fold
+              (grep (invert (asum (map (has . text . showPath) files)))
+                (inproc "git" ["show", "--stat", commit] empty))
+              countLines
+            return (linePresent == 0)
         Just files -> do
           linePresent <- fold
             (grep (asum (map (has . text . showPath) files))
@@ -46,22 +53,22 @@ checkLocalChangelogF fmt writeLog GitInfo{..} path watchFilePaths = do
         case pull of
           Nothing -> do
             message <- commitMessage Commit commit
-            changelogIsUp fmt writeLog gitRemoteUrl commit Commit message path
+            changelogIsUp fmt writeLog gitRemoteUrl commit Commit message changelogChangelog
           Just pnum -> do
             message <- commitMessage PR commit
-            changelogIsUp fmt writeLog gitRemoteUrl pnum PR message path
+            changelogIsUp fmt writeLog gitRemoteUrl pnum PR message changelogChangelog
 
 -- |This is actually part if '@Main@'
 -- Check given changelog regarding options.
 checkChangelogWrap :: Options -> GitInfo -> ChangelogConfig -> IO Bool
-checkChangelogWrap Options{..} git ChangelogConfig{..} = do
+checkChangelogWrap Options{..} git config@ChangelogConfig{..} = do
   if (optUpdateChangelog && optFormat == WarnSimple)
     then do
       failure "--update-changelog does not work with --format=simple (try --format=suggest instead)"
       return False
     else do
       when optFromBC $ printf ("Checking "%fp%" from start of project\n") changelogChangelog
-      upToDate <- checkLocalChangelogF optFormat optUpdateChangelog git changelogChangelog changelogWatchFiles
+      upToDate <- checkLocalChangelogF optFormat optUpdateChangelog git config
       if upToDate
         then coloredPrint Green (showPath changelogChangelog <> " is up to date.\n")
         else do
