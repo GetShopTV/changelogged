1a2ef419b7775d27255d5fccec09733529113bea
Vitalii Guzeev <viviag@yandex.ru>
2018-08-10 12:00:46 +0300

    cd to project root while working (closes #118)
    It's also assumed user doesn't have files or directories called same as the whole project.
    It's impossible for most release-level projects.

--- src/Changelogged/Common/Utils/IO.hs
+++ src/Changelogged/Common/Utils/IO.hs
@@ -3,14 +3,17 @@ module Changelogged.Common.Utils.IO where
 import Control.Monad (when)
 import Data.Aeson (ToJSON)
 import Data.Text (Text)
+import Data.String.Conversions
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import Data.Monoid ((<>))
 import qualified Data.Yaml as Yaml
 
 import System.Console.ANSI
+import qualified Filesystem.Path.CurrentOS as Path
 
 import Turtle.Format
+import Turtle (pwd, cd)
 
 import Changelogged.Common.Types
 
@@ -57,3 +60,18 @@ debugYaml title val = debug (title <> "\n" <> Text.decodeUtf8 (Yaml.encode val))
 versionP :: Version -> Appl ()
 versionP (Version ver) = coloredPrint Green $
   "VERSION: " <> ver <> "\n"
+
+splitPwdBy :: Text -> IO (Maybe Path.FilePath)
+splitPwdBy gitProjectName = do
+  curDirText <- cs . Path.encodeString <$> pwd
+  return $ case Path.fromText . fst $ Text.breakOnEnd gitProjectName curDirText of
+    "" -> Nothing
+    path -> Just path
+
+withDir :: MonadIO m => Path.FilePath -> m a -> m a
+withDir dir action = do
+  prev <- pwd
+  liftIO $ cd dir
+  res <- action
+  liftIO $ cd prev
+  return res
