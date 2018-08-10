13da96f07b2142bad68c236407059786ae8c99e9
Vitalii Guzeev <viviag@yandex.ru>
2018-08-10 13:14:31 +0300

    Fix --force

--- src/Changelogged/Versions/Bump.hs
+++ src/Changelogged/Versions/Bump.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE MultiWayIf #-}
 module Changelogged.Versions.Bump where
 
 import Turtle
@@ -67,21 +66,21 @@ generateVersionByChangelog logConfig@ChangelogConfig{..} = do
 bumpVersions :: Bool -> ChangelogConfig -> Appl ()
 bumpVersions upToDate config@ChangelogConfig{..} = do
   Options{..} <- asks envOptions
-  if
-    | not upToDate && not optForce ->
-        failure $ "cannot bump versions because " <> format fp changelogChangelog <> " is out of date.\nUse --no-check to skip changelog checks.\nUse --force to force bump version."
-    | not upToDate && optForce ->
-        warning $ format fp changelogChangelog <> " is out of date. Bumping versions anyway due to --force."
-    | otherwise -> (do
-        newVersion <- case optChangeLevel of
-          Just lev -> generateVersion lev config
-          Nothing -> generateVersionByChangelog config
+  if (upToDate || optForce)
+    then (do
+      when optForce $ warning $ format fp changelogChangelog <> " is out of date. Bumping versions anyway due to --force."
+      newVersion <- case optChangeLevel of
+        Just lev -> generateVersion lev config
+        Nothing -> generateVersionByChangelog config
 
-        case newVersion of
-          Nothing -> return ()
-          Just version -> case changelogVersionFiles of
-            Just versionFiles -> do
-              mapM_ (bumpPart version) versionFiles
-              headChangelog version changelogChangelog
-            Nothing -> warning "no files specified to bump versions in"
-        ) `catch` (\(ex :: PatternMatchFail) -> failure (showText ex))
+      case newVersion of
+        Nothing -> return ()
+        Just version -> case changelogVersionFiles of
+          Just versionFiles -> do
+            mapM_ (bumpPart version) versionFiles
+            headChangelog version changelogChangelog
+          Nothing -> warning "no files specified to bump versions in"
+      ) `catch` (\(ex :: PatternMatchFail) -> failure (showText ex))
+    else failure $ "cannot bump versions because " <> format fp changelogChangelog <> " is out of date.\n" <>
+                   "Use --no-check to skip changelog checks.\n" <>
+                   "Use --force to force bump version."
